baeab5a89a5177c558f7e8946d629a18ac7bfa1c
JorgeBorras18 <104654988+JorgeBorras18@users.noreply.github.com>
2024-05-04 17:02:41 +0200

    jordi judith

--- backend/django_backend/app/views.py
+++ backend/django_backend/app/views.py
@@ -10,12 +10,17 @@ import json
 
 # Create your views here.
 
-    
+
 @api_view(['GET'])
 def getTest(request, format=None):
     return Response({"GET" : "WORKS"}, status=status.HTTP_200_OK)
 
+
 @api_view(['POST'])
 def postTest(request, format=None): 
-    data = request.data.get("question")
-    return Response({"POST" : data}, status=status.HTTP_200_OK)
+    question = request.data.get("question")
+    language = request.data.get("language")
+
+    # your code goes here
+
+    return Response({"POST" : { "language": language, "question": question}}, status=status.HTTP_200_OK)
