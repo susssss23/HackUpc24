1ff2033938218f416731dd800d53f019559c42df
susssss23 <susannajaner@gmail.com>
2024-05-04 15:17:51 +0200

    basic frontend fet, començant speech to test

--- frontend/lib/presentation/screens/my_home_page.dart
+++ frontend/lib/presentation/screens/my_home_page.dart
@@ -12,44 +12,126 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late ControladorPresentacio _controladorPresentacio;
-  int _counter = 0;
+
+  bool defaultState = false;
+  bool selectedMirco = false; //si no es selected micro es selectedText
+
+  late String textEntered;
 
   _MyHomePageState(ControladorPresentacio controladorPresentacio) {
     _controladorPresentacio = controladorPresentacio;
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    textEntered = '';
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromRGBO(33, 33, 33, 1),
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("titol"),
+        title: const Text("Hack24"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const Padding(
+              padding: EdgeInsets.only(right: 340.0, bottom: 25.0),
+              child: Text(
+                'Ask me anything',
+                style: TextStyle(
+                  color: Colors.purple,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Monsterrat',
+                  fontSize: 20.0,
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              child: _buildEnterBar(),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+
+  Widget _buildEnterBar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width * 0.5,
+          //equivalen a  50% de la pantalla
+          child: TextField(
+            maxLines: 4,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              hintText: 'Enter...',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+                borderSide: const BorderSide(width: 5.0, color: Colors.purple),
+              ),
+            ),
+            onChanged: (value) {
+              textEntered = value;
+            },
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(10.0),
+        ),
+        Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.send),
+                color: Colors.white,
+                iconSize: 30,
+                onPressed: () {
+                  setState(() {
+                    defaultState = false;
+                    selectedMirco = true;
+                  });
+                },
+                padding: const EdgeInsets.all(9.0),
+                splashRadius: 20,
+                constraints: BoxConstraints(),
+              ),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.purple,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.mic_rounded),
+                color: Colors.white,
+                iconSize: 30,
+                onPressed: () {
+                  setState(() {
+                    defaultState = false;
+                    selectedMirco = true;
+                  });
+                },
+                padding: const EdgeInsets.all(9.0),
+                splashRadius: 20,
+                constraints: BoxConstraints(),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -5,15 +5,19 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:my_web_app/main.dart';
+import 'package:my_web_app/presentation/controlador_presentacio.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    ControladorPresentacio controladorPresentacio = ControladorPresentacio();
+    await tester.pumpWidget(MyApp(
+      controladorPresentacio: controladorPresentacio,
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
@@ -27,4 +31,4 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
-}
+}*/
